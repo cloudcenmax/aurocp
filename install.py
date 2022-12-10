9ca4414456a35ae1ce2614d231135ce37f8b6f49
Kavin <rajkavinchetty@gmail.com>
2022-12-10 16:31:26 +0530

    Update install.py

--- install.py
+++ install.py
@@ -2,3 +2,10 @@
 import os
 
 os.system("echo Hello from the other side!")
+os.system('yum update httpd')
+os.system('yum install httpd')
+os.system('firewall-cmd --permanent --add-service=http')
+os.system('firewall-cmd --permanent --add-service=https')
+os.system('firewall-cmd --reload')
+os.system('systemctl start httpd')
+os.system('systemctl status httpd')
